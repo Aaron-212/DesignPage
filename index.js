159640bb7e0e3bd0596b279d2c3d6f67938aa923
Aaron Ruan <aaron212cn@outlook.com>
2023-12-25 21:52:23 +0800

    Fix Details #2

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Navigation Bar animation
-const tl = gsap.timeline();
-
-tl.fromTo(".wrapper",
+const scrolltl = new gsap.timeline();
+scrolltl.fromTo(".wrapper",
     {
         height: "96px",
         maxWidth: "800px"
@@ -14,12 +13,12 @@ tl.fromTo(".wrapper",
     {
         backgroundColor: "rgba(0, 0, 0, 0)",
         borderBottomColor: "rgba(128, 128, 128, 0)",
-        backdropFilter:" blur(0px)",
+        backdropFilter: " blur(0px)",
     },
     {
         backgroundColor: "rgba(0, 0, 0, 0.5)",
         borderBottomColor: "rgba(128, 128, 128, 1)",
-        backdropFilter:" blur(50px)",
+        backdropFilter: " blur(50px)",
     },
     '<'
 ).fromTo("#navbar a",
@@ -32,7 +31,7 @@ ScrollTrigger.create({
     trigger: ".wrapper",
     start: 0,
     end: 64,
-    animation: tl,
+    animation: scrolltl,
     scrub: 0.5,
     repeat: -1,
 })
@@ -42,11 +41,11 @@ var container = document.getElementById("container");
 
 for (var re = 0; re < 3; re++) {
     for (var i = 0; i <= 10; i++) {
-        var testTemplate = `Font Size is ${10 + i/5}`;
+        var testTemplate = `Font Size is ${10 + i / 5}`;
         var test = document.createElement("p");
         test.classList.add("test");
         test.innerHTML = testTemplate;
-        test.style.fontSize = (10 + i/5) + "px";
+        test.style.fontSize = (10 + i / 5) + "px";
         container.appendChild(test);
     }
 }
